76ae10135bf0fd2813930678618d221c5b83578f
Nikolay Nikolov <nikolaynikolov91@gmail.com>
2021-07-21 16:11:07 +0300

    fix plane movements

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,7 @@ const planeImg = PIXI.Texture.from("./images/plane.png");
 const planeSprite = new PIXI.Sprite(planeImg);
 const plane = new Hero("./images/plane.png", planeSprite.x, planeSprite.y);
 const myText = new PIXI.Text(`SCORE: ${plane.score}`);
+const keys: any = {};
 
 class Enemy {
   isDead = false;
@@ -49,44 +50,33 @@ class Enemy {
   }
 }
 
-planeSprite.width = 100;
-planeSprite.height = 70;
+planeSprite.width = (app.renderer.height * 11) / 100;
+planeSprite.height = (app.renderer.height * 7) / 100;
 planeSprite.x = 200;
 
+const moveSide = (app.renderer.height * 7) / 100;
 const move: any = {
-  ArrowUp: (planeSprite.y -= 100),
+  ArrowUp: (planeSprite.y -= moveSide),
 
-  ArrowDown: (planeSprite.y += 100),
+  ArrowDown: (planeSprite.y += moveSide),
 
-  ArrowLeft: (planeSprite.x -= 100),
+  ArrowLeft: (planeSprite.x -= moveSide),
 
-  ArrowRight: (planeSprite.x += 100),
+  ArrowRight: (planeSprite.x += moveSide),
 };
 
-document.addEventListener("keydown", logKey);
+document.addEventListener("keydown", keysDown);
+document.addEventListener("keyup", keysUp);
 
-function logKey(e: any): any {
-  move[e.code];
-  console.log(e.code);
-
-  switch (e.code) {
-    case "ArrowUp":
-      planeSprite.y -= 10;
-      break;
-    case "ArrowDown":
-      planeSprite.y += 10;
-      break;
-    case "ArrowLeft":
-      planeSprite.x -= 10;
-      break;
-    case "ArrowRight":
-      planeSprite.x += 10;
-      break;
-
-    default:
-      break;
-  }
+function keysDown(e: KeyboardEvent): any {
+  keys[e.key] = true;
 }
+
+function keysUp(e: any) {
+  keys[e.key] = false;
+}
+
+app.ticker.add(gameLoop);
 
 const backgroundTexture = PIXI.Texture.from("./images/background-img.jpg");
 const bgSprite = new PIXI.TilingSprite(
@@ -100,17 +90,38 @@ app.ticker.add(function () {
 });
 
 const firstEnemy = PIXI.Sprite.from("./images/enemy.png");
-firstEnemy.width = 180;
-firstEnemy.height = 110;
-firstEnemy.x = 1800;
-firstEnemy.y = 780;
+firstEnemy.width = app.renderer.width / 10;
+firstEnemy.height = (app.renderer.height * 11) / 100;
+
+firstEnemy.x = app.renderer.width;
+firstEnemy.y = (app.renderer.height * 80.5) / 100;
 
 app.stage.addChild(bgSprite);
 app.stage.addChild(myText);
 app.stage.addChild(planeSprite);
 app.stage.addChild(firstEnemy);
 
+function gameLoop() {
+  if (keys["ArrowUp"]) {
+    planeSprite.y -= 10;
+  }
+  if (keys["ArrowDown"]) {
+    planeSprite.y += 10;
+  }
+  if (keys["ArrowLeft"]) {
+    planeSprite.x -= 10;
+  }
+  if (keys["ArrowRight"]) {
+    planeSprite.x += 10;
+  }
+}
+
 app.ticker.add((delta) => loop(delta));
+
 function loop(delta: any) {
+  //console.log(firstEnemy.y);
   firstEnemy.x -= 1;
+  if (firstEnemy.x < 0) {
+   // firstEnemy.destroy(true);
+  }
 }
